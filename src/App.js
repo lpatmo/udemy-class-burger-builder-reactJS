f5711d29c3b0db3bd4ad652261d35c784ab72258
Susan Tan <stan@bepress.com>
2019-02-20 16:58:00 -0800

    Create ingredients, transform a list of ingredients to ingredient components, add proptype

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 // import logo from './logo.svg';
+
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Layout>
+        <BurgerBuilder />
+       </Layout>
       </div>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
+
 const layout = (props) => (
-  <div>Toolbar, SideDrawer, Backdrop</div>
-  <main>
-    {props.children}
-  </main>
-)
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default layout;
